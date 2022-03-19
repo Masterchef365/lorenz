f2fe8c86b7afd8252d6f2938d765f41e5cfd801b
Masterchef365 <duncan.freeman1@gmail.com>
2022-03-19 01:04:30 -0700

    Better results with the Runge-Kutta method

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 use idek::{prelude::*, IndexBuffer, MultiPlatformCamera};
+use ultraviolet::Vec3;
 
 fn main() -> Result<()> {
-    launch::<_, TriangleApp>(Settings::default().vr_if_any_args())
+    launch::<_, LorenzViz>(Settings::default().vr_if_any_args())
 }
 
-struct TriangleApp {
+struct LorenzViz {
     verts: VertexBuffer,
     indices: IndexBuffer,
     camera: MultiPlatformCamera,
     lines_shader: Shader,
 }
 
-impl App for TriangleApp {
+impl App for LorenzViz {
     fn init(ctx: &mut Context, platform: &mut Platform, _: ()) -> Result<Self> {
         let (vertices, indices) = lorenz_lines(
-            [1., 1., 1.], 
-            [10., 28., 8. / 3.], 
-            0.01, 
-            40, 
+            [1., 1., 1.].into(),
+            [5., 28., 8. / 3.],
+            0.01,
+            400,
             [1.; 3],
             1. / 10.,
         );
-        dbg!(&vertices);
 
         Ok(Self {
             verts: ctx.vertices(&vertices, false)?,
@@ -56,18 +56,27 @@ impl App for TriangleApp {
 }
 
 fn lorenz_lines(
-    initial_pos: [f32; 3],
+    initial_pos: Vec3,
     coeffs: [f32; 3],
     dt: f32,
     n: usize,
     color: [f32; 3],
     scale: f32,
 ) -> (Vec<Vertex>, Vec<u32>) {
-    let vertices: Vec<Vertex> = lorenz(initial_pos, coeffs, dt)
-        .map(|pos| pos.map(|v| v * scale))
-        .map(|pos| Vertex::new(pos, color))
-        .take(n)
-        .collect();
+    let mut ode = RungeKutta::new(0., initial_pos, dt);
+
+    let f = |_, pos: Vec3| lorenz(pos.into(), coeffs).into();
+
+    ode.step(f);
+
+    let vertices: Vec<Vertex> = std::iter::from_fn(|| {
+        ode.step(f);
+        Some(ode.y().into())
+    })
+    .map(|pos: [f32; 3]| pos.map(|v| v * scale))
+    .map(|pos| Vertex::new(pos, color))
+    .take(n)
+    .collect();
     let indices: Vec<u32> = (0..)
         .map(|i| (i + 1) / 2)
         .take((vertices.len() - 1) * 2)
@@ -75,17 +84,41 @@ fn lorenz_lines(
     (vertices, indices)
 }
 
-fn lorenz(
-    [mut x, mut y, mut z]: [f32; 3],
-    [sigma, rho, beta]: [f32; 3],
-    dt: f32,
-) -> impl Iterator<Item = [f32; 3]> {
-    std::iter::from_fn(move || {
-        let next_x = dt * sigma * (x - y);
-        let next_y = dt * (x * (rho - z) - y);
-        z = dt * (x * y - beta * z);
-        x = next_x;
-        y = next_y;
-        Some([x, y, z])
-    })
+fn lorenz([x, y, z]: [f32; 3], [sigma, rho, beta]: [f32; 3]) -> [f32; 3] {
+    [sigma * (x - y), x * (rho - z) - y, x * y - beta * z]
+}
+
+pub struct RungeKutta {
+    x: f32,    // t
+    y: Vec3,   // (x, y, z)
+    step: f32, // dt
+}
+
+impl RungeKutta {
+    pub fn new(x_min: f32, y_init: Vec3, step: f32) -> Self {
+        Self {
+            x: x_min,
+            y: y_init,
+            step,
+        }
+    }
+
+    /// Returns the result of fourth-order Runge-Kutta method for a given function
+    /// f(x, y) -> dy/dx
+    pub fn step(&mut self, f: impl Fn(f32, Vec3) -> Vec3) {
+        let k1 = self.step * f(self.x, self.y);
+        let k2 = self.step * f(self.x + self.step / 2., self.y + k1 / 2.);
+        let k3 = self.step * f(self.x + self.step / 2., self.y + k2 / 2.);
+        let k4 = self.step * f(self.x + self.step, self.y + k3);
+        self.y += (k1 + 2. * k2 + 2. * k3 + k4) / 6.;
+        self.x += self.step;
+    }
+
+    pub fn x(&self) -> f32 {
+        self.x
+    }
+
+    pub fn y(&self) -> Vec3 {
+        self.y
+    }
 }
